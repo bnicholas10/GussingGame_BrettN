91267da84e2e8d4b80a59258e86a3ab75d96dc83
Brett Nicholas <bnicholas10@gmail.com>
2022-07-17 01:31:26 -0400

    submitted

--- main.js
+++ main.js
@@ -20,39 +20,75 @@ guessButton.addEventListener('click', function(){
         hintButton.disabled = true
         window.alert("Out of attempts!")
     } 
-    if (difference === 0){
+    if(numIn === "" || numIn > 100){
+        window.alert("Thats not a valid number!")
+        guesses -= 1;
+    }
+    else if (difference === 0){
         tempReporter.textContent = (`Winner!`)
         tempReporter.style.boxShadow = "0px 0px 20px gold, 0px 0px 20px inset gold"
         guessButton.disabled = true
         hintButton.disabled = true
+        // Push to guess tracker
+        let guess = document.createElement('li');
+        guess.innerHTML = numIn;
+        // Alternative option to add text
+        // guess.appendChild(document.createTextNode(numIn))
+        document.getElementById('guessTracker').appendChild(guess);
     }
-    else if (difference < 6){
+    else if (difference <= 3){
         tempReporter.textContent = (`Blazing!!`)
         tempReporter.style.boxShadow = "0px 0px 20px darkred, 0px 0px 20px inset darkred"
+        // Push to guess tracker
+        let guess = document.createElement('li');
+        guess.innerHTML = numIn;
+        // Alternative option to add text
+        // guess.appendChild(document.createTextNode(numIn))
+        document.getElementById('guessTracker').appendChild(guess);
     }
-    else if (difference < 26){
+    else if (difference <= 10){
         tempReporter.textContent = (`Hot!`)
         tempReporter.style.boxShadow = "0px 0px 20px red, 0px 0px 20px inset red"
+        // Push to guess tracker
+        let guess = document.createElement('li');
+        guess.innerHTML = numIn;
+        // Alternative option to add text
+        // guess.appendChild(document.createTextNode(numIn))
+        document.getElementById('guessTracker').appendChild(guess);
     }
-    else if (difference < 51){
+    else if (difference <= 40){
         tempReporter.textContent = (`Warm`)
         tempReporter.style.boxShadow = "0px 0px 20px orange, 0px 0px 20px inset orange"
+        // Push to guess tracker
+        let guess = document.createElement('li');
+        guess.innerHTML = numIn;
+        // Alternative option to add text
+        // guess.appendChild(document.createTextNode(numIn))
+        document.getElementById('guessTracker').appendChild(guess);
     }
-    else if (difference < 76){
+    else if (difference <= 60){
         tempReporter.textContent = (`Cold`)
         tempReporter.style.boxShadow = "0px 0px 20px lightblue, 0px 0px 20px inset lightblue"
+        // Push to guess tracker
+        let guess = document.createElement('li');
+        guess.innerHTML = numIn;
+        // Alternative option to add text
+        // guess.appendChild(document.createTextNode(numIn))
+        document.getElementById('guessTracker').appendChild(guess);
     }
-    else{
+    else {
         tempReporter.textContent = (`Freezing!`)
         tempReporter.style.boxShadow = "0px 0px 20px darkblue, 0px 0px 20px inset darkblue"
+        // Push to guess tracker
+        let guess = document.createElement('li');
+        guess.innerHTML = numIn;
+        // Alternative option to add text
+        // guess.appendChild(document.createTextNode(numIn))
+        document.getElementById('guessTracker').appendChild(guess);
     }
     
-    // Push to guess tracker
-    let guess = document.createElement('li');
-    guess.innerHTML = numIn;
-    // Alternative option to add text
-    // guess.appendChild(document.createTextNode(numIn))
-    document.getElementById('guessTracker').appendChild(guess);
+
+    document.getElementById("numIn").value = ""
     
     //testing
     console.log(`Guesses ` + guesses)
@@ -81,7 +117,10 @@ resetButton.addEventListener(`click`, function() {
 // hint button
 hintButton.addEventListener('click', function() {
     let numIn = document.getElementById("numIn").value
-    if (numIn < randomNum){
+    if (numIn === ""){
+        window.alert("Type in your guess!")
+    }
+    else if (numIn < randomNum){
         tempReporter.textContent = (`try higher!`)
     }
     else if (numIn > randomNum){
@@ -91,11 +130,12 @@ hintButton.addEventListener('click', function() {
         tempReporter.textContent = (`take a guess!`)
     }
     // console.log(numIn)
+    // improve hint to include number range +- 10 from randomNum
 })
 
 
 
-
+//Create indication of attempts left
 //Guess a number 1-100
 //Indicate if guess is hot or cold
 //Keep track of previous guesses
